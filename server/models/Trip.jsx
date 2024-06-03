2bd5b8dad88e9e2cf5614d3070e2858f25446b62
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-06-02 20:25:52 -0500

    updated the model files

---
+++ server/models/Trip.jsx
@@ -0,0 +1,32 @@
+// Model for trips
+const {Schema, model}= require('mongoose');
+
+const tripSchema= new Schema({
+    userId:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    startDate:{
+        type: Date,
+        required: true,
+    },
+    endDate:{
+        type: Date,
+        required: true,
+    },
+    description:{
+        type: String,
+    },
+},
+{
+    timestamps: true,
+});
+
+const Trip= model('Trip', tripSchema);
+
+module.exports= Trip;
---
+++ server/models/User.jsx
@@ -0,0 +1,47 @@
+// Model for our users
+const {Schema, model}= require('mongoose');
+const bcrypt= require('bcrypt');
+
+const userSchema= new Schema({
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+},
+{
+    timestamps: true,
+});
+
+// To hash the password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // To compare the password
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+
+const User= model('User', userSchema);
+
+module.exports= User;
+
+// Saltround us just another security parameter.
