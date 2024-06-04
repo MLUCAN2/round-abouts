9fb766cfc743a14cdf30ca0cad98bccaa7698487
dwhitejr3 <dwhitejr3@gmail.com>
2024-06-04 10:46:09 -0500

    main.jsx, vite.config,

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import Home from './pages/Home.jsx';
 import Activities from './pages/Activities.jsx';
 import CreateTrip from './pages/CreateTrip.jsx';
 import Login from './pages/Login.jsx';
@@ -47,6 +47,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
+   <RouterProvider router={router} />  
+  </React.StrictMode>
 )
