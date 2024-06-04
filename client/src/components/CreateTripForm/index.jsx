2889ab73f20255fae799b3f981e0e88619c8bfe1
dwhitejr3 <dwhitejr3@gmail.com>
2024-06-04 11:45:30 -0500

    start create trip form

---
+++ client/src/components/CreateTripForm/index.jsx
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+
+export default CreateTripForm = () => {
+    const [tripText, setTripText] = useState('')
+}
--- client/src/pages/CreateTrip.jsx
+++ client/src/pages/CreateTrip.jsx
@@ -1,3 +1,10 @@
+import { useState } from 'react';
+
+
 export default function CreateTrip () {
-    
+    return (
+        <div className="Title">
+            <h1>Create your trip here!</h1>
+        </div>
+    )
 }
