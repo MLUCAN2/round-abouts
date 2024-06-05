c1a4cfffa8895eb2ee1f03e932b5f645d41b29c9
dwhitejr3 <dwhitejr3@gmail.com>
2024-06-05 10:19:40 -0500

    createTripFrom

--- client/src/pages/CreateTrip.jsx
+++ client/src/pages/CreateTrip.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-
+import CreateTripForm from "../components/CreateTripForm/index"
 
 export default function CreateTrip () {
     return (
         <div className="Title">
             <h1>Create your trip here!</h1>
+            <CreateTripForm/>
         </div>
     )
 }
