2d3fb23ce24a6e712728f66b2b3d09b5dc85908e
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-06-06 14:38:16 -0500

    debug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -46,7 +46,6 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
    <RouterProvider router={router} />  
-  </React.StrictMode>
+
 )
